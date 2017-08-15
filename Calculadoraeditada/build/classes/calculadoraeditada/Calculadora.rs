4f199b03895812a0f60451be9bf28e8b41c75c69
IamNez2000 <nezarrell2000@gmail.com>
2017-08-15 08:55:35 -0500

    Some text

---
+++ Calculadoraeditada/build/classes/calculadoraeditada/Calculadora.rs
@@ -0,0 +1,3 @@
+edu.ipn.cecyt9.calculadora.Calculadora
+edu.ipn.cecyt9.calculadora.Calculadora$1
+edu.ipn.cecyt9.calculadora.Calculadora$2
---
+++ Calculadoraeditada/src/calculadoraeditada/Calculadoraeditada.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraeditada;
+
+/**
+ *
+ * @author emmanuel
+ * @version 2.0
+ * @date 15-08-2017
+ * @see : Calculadora
+ */
+public class Calculadoraeditada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+    }
+    
+}
